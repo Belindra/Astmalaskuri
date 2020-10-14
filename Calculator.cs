6f602c1efa33e816657c3ce8b41b7b2d9987e35e
Sirene <s2000583@edu.bc.fi>
2020-10-14 09:12:50 +0300

    Päivitä koodi

--- Calculator.cs
+++ Calculator.cs
@@ -6,75 +6,151 @@ namespace Asthma_Calc
 {
     class Calculator
     {
-        int medicine;
-        int portion;
+        int exit = 0;
+
         List<int> totalportion = new List<int>();
         List<int> usedportion = new List<int>();
         public void Medicine()
-        {      
+        {
             var strPath = @"Medicine.csv";
 
-            Console.WriteLine("Kumpaa astmalääkettä käytit?");
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.WriteLine("1. Ventoline");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("2. Flixotide");
-            medicine = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-
-            Console.WriteLine("Kuinka monta annosta otit?");
-            Console.WriteLine("1");
-            Console.WriteLine("2");
-            portion = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-
             CSV.ReadingCSV(totalportion, usedportion, strPath);
-        
-            if (medicine == 1)
+            Console.WriteLine("Lunan astmalääkelaskuri\n");
+
+            /* int unusedvl = totalportion[1] - usedportion[1];
+             int unusedfx = totalportion[0] - usedportion[0];
+
+             if (usedportion[0] >= 10)
+             {
+                 Console.ForegroundColor = ConsoleColor.DarkYellow;
+                 Console.WriteLine($"Huom! Flixotidea on jäljellä enää {unusedfx} annosta!");
+             }
+
+             if (usedportion[1] >= 10)
+             {
+                 Console.ForegroundColor = ConsoleColor.DarkCyan;
+                 Console.WriteLine($"Huom! Ventolinea on jäljellä enää {unusedvl} annosta!\n");
+             }
+             Console.ResetColor();*/
+
+            do
             {
-                if (portion == 1)
-                {
-                    usedportion[0] = usedportion[0] + 1;
-                    int unused = totalportion[0] - usedportion[0];
-                    Console.WriteLine($"Ventolinea on jäljellä {unused} annosta");
-                        
-                }
+                int choice = MenuAndError.Menu();
 
-                else if (portion == 2)
+                switch (choice)
                 {
-                    usedportion[0] = usedportion[0] + 2;
-                    int unused = totalportion[0] - usedportion[0];
-                    Console.WriteLine($"Ventolinea on jäljellä {unused} annosta");
-                }
+                    case 1:
+                        int unusedvl = totalportion[1] - usedportion[1];
+                        int unusedfx = totalportion[0] - usedportion[0];
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write($"Flixotidea on jäljellä {unusedfx} annosta,");
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.Write($"Ventolinea on jäljellä {unusedvl} annosta\n\n");
+                        Console.ResetColor();
+                        break;
+                    case 2:
+                        Console.WriteLine("Kumpaa astmalääkettä käytit?\n");
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("1. Flixotide");
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("2. Ventoline");
+                        Console.ResetColor();
+                        int medicine = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
 
-                else
-                    Console.WriteLine("Painoit väärää nappia, pässi");
+                        Console.WriteLine("Kuinka monta annosta otit?\n");
+                        Console.WriteLine("1");
+                        Console.WriteLine("2");
+                        int portion = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+
+                        if (medicine == 1)
+                        {
+                            if (portion == 1)
+                            {
+                                usedportion[0] = usedportion[0] + 1;
+                                int unused = totalportion[0] - usedportion[0];
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.WriteLine($"Flixotidea on jäljellä {unused} annosta\n");
+                                Console.ResetColor();
+
+                            }
+
+                            else if (portion == 2)
+                            {
+                                usedportion[0] = usedportion[0] + 2;
+                                int unused = totalportion[0] - usedportion[0];
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.WriteLine($"Flixotidea on jäljellä {unused} annosta\n");
+                                Console.ResetColor();
+                            }
+
+                            else
+                                MenuAndError.PrintError();
+                        }
+
+                        else if (medicine == 2)
+                        {
+                            if (portion == 1)
+                            {
+                                usedportion[1] = usedportion[1] + 1;
+                                int unused = totalportion[1] - usedportion[1];
+                                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                                Console.WriteLine($"Ventolinea on jäljellä {unused} annosta\n");
+                                Console.ResetColor();
+
+                            }
+
+                            else if (portion == 2)
+                            {
+                                usedportion[1] = usedportion[1] + 2;
+                                int unused = totalportion[1] - usedportion[1];
+                                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                                Console.WriteLine($"Ventolinea on jäljellä {unused} annosta\n");
+                                Console.ResetColor();
+                            }
+
+                            else
+                            {
+                                MenuAndError.PrintError();
+                            }
+                        }
+
+                        else
+                        {
+                            MenuAndError.PrintError();
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Kumman lääkkeen haluat resetoida?\n");
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("1. Flixotide");
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("2. Ventoline");
+                        Console.ResetColor();
+                        int reset = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+
+                        if (reset == 1)
+                        {
+                            usedportion[0] = 0;
+                        }
+
+                        else
+                        {
+                            usedportion[1] = 0;
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Ohjelma loppuu. Mene pois");
+                        exit = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Väärä valinta, yritä uudelleen");
+                        break;
+                }
             }
-
-            else if (medicine == 2)
-            {
-                if (portion == 1)
-                {
-                    usedportion[0] = usedportion[0] + 1;
-                    int unused = totalportion[1] - usedportion[1];
-                    Console.WriteLine($"Ventolinea on jäljellä {unused} annosta");
-
-                }
-
-                else if (portion == 2)
-                {
-                    usedportion[1] = usedportion[1] + 2;
-                    int unused = totalportion[1] - usedportion[1];
-                    Console.WriteLine($"Ventolinea on jäljellä {unused} annosta");
-                }
-
-                else
-                    Console.WriteLine("Painoit väärää nappia, pässi");
-            }
-
-            else
-                Console.WriteLine("Painoit väärää nappia, pässi");
-
+            while (exit == 0);
             CSV.WritingCSV(totalportion, usedportion, strPath);
         }
     }
